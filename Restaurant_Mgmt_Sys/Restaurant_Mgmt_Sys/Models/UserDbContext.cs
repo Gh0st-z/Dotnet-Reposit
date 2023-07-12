123f779aa8698a9d0be5ac5b120819d385b0295b
Gh0st-z <shinigamikami446@gmail.com>
2023-07-12 18:22:45 +0545

    saved

---
+++ Restaurant_Mgmt_Sys/Restaurant_Mgmt_Sys/Models/UserDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Restaurant_Mgmt_Sys.Models
+{
+    public class UserDbContext:DbContext
+    {
+        public UserDbContext(DbContextOptions<UserDbContext> options): base(options) 
+        { }
+        
+
+    }
+}
