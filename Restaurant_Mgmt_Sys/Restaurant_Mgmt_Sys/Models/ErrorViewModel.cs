de2acbfc58359d644d167291bbbf68548749e468
Gh0st-z <139203744+Gh0st-z@users.noreply.github.com>
2023-07-13 09:30:54 +0545

    Delete Restaurant_Mgmt_Sys directory

--- Restaurant_Mgmt_Sys/Restaurant_Mgmt_Sys/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Restaurant_Mgmt_Sys.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
